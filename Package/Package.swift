629e47db0973f7e73f2642abd9e84d2cd4dc2556
David Arrufat <david.arrufat@agoraimages.com>
2023-10-01 16:29:02 +0200

    Package base structure with recipes searcher and domain

--- Package/Package.swift
+++ Package/Package.swift
@@ -1,23 +1,68 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Package",
+    platforms: [
+        .iOS("17.0")
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // MARK: - App Features
         .library(
-            name: "Package",
-            targets: ["Package"]),
+            name: "RecipesSearcher",
+            targets: ["RecipesSearcher"]),
+
+        // MARK: - Domain Layer
+        .library(
+            name: "Domain",
+            targets: ["Domain"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", 
+                 from: "1.13.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        // MARK: - Application
         .target(
-            name: "Package"),
+            name: "RecipesSearcher",
+            dependencies: ["Domain"],
+            path: "Sources/Application/RecipesSearcher"
+        ),
         .testTarget(
-            name: "PackageTests",
-            dependencies: ["Package"]),
+            name: "RecipesSearcherTests",
+            dependencies: ["RecipesSearcher"], // ADD NIMBLE
+            path: "Tests/Unit/RecipesSearcherTests"
+        ),
+        .testTarget(
+            name: "RecipesSearcherSnapshotTests",
+            dependencies: [
+                "RecipesSearcher",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
+            ],
+            path: "Tests/Snapshot/RecipesSearcherSnapshotTests",
+            exclude: ["__Snapshots__"]
+        ),
+        .target(
+            name: "Domain",
+            dependencies: ["Assembly"],
+            path: "Sources/Application/Domain"
+        ),
+        .testTarget(
+            name: "DomainTests",
+            dependencies: ["Domain"], // ADD NIMBLE
+            path: "Tests/Unit/DomainTests"
+        ),
+
+        // MARK: - Core
+        .target(
+            name: "Assembly",
+            path: "Sources/Core/Assembly"
+        ),
+        .testTarget(
+            name: "AssemblyTests",
+            dependencies: ["Assembly"], // ADD NIMBLE
+            path: "Tests/Unit/AssemblyTests"
+        ),
     ]
 )
